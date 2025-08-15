4011387c489c396b3917ed2e47671c665f381c19
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-08-15 16:47:16 +0200

    Windows updates

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,14 +1,8 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-const fs = require('fs');
 const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const { withMetroConfig } = require('react-native-monorepo-config');
 
-const rnwPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
-);
-
-//
+const root = path.resolve(__dirname, '..');
 
 /**
  * Metro configuration
@@ -16,30 +10,7 @@ const rnwPath = fs.realpathSync(
  *
  * @type {import('metro-config').MetroConfig}
  */
-
-const config = {
-  //
-  resolver: {
-    blockList: exclusionList([
-      // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-      ),
-      // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
-      new RegExp(`${rnwPath}/build/.*`),
-      new RegExp(`${rnwPath}/target/.*`),
-      /.*\.ProjectImports\.zip/,
-    ]),
-    //
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withMetroConfig(getDefaultConfig(__dirname), {
+  root,
+  dirname: __dirname,
+});
